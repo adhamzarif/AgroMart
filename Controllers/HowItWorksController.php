2d90949a51829d080175cbb0145e85fc793ae6b3
Adham Zarif <zarifseam52@gmail.com>
2026-07-30 13:58:47 +0600

    Reduce working tree to new stack; PHP app preserved in history + Downloads

--- Controllers/HowItWorksController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class HowItWorksController extends Controller {
-    public function index() {
-        $title = "কীভাবে কাজ করে | AgroFin";
-        $this->view('how_it_works/index', compact('title'));
-    }
-}
